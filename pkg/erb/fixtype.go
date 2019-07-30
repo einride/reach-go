580bc25e9bdb1c080b852bee8d2a9faab972dbad
Oscar Söderlund <odsod@users.noreply.github.com>
2019-07-30 08:32:44 +0200

    Use Makefile to generate Go code
    This speeds up the build by only re-generating code when the dependent
    file has changed.

--- pkg/erb/fixtype.go
+++ pkg/erb/fixtype.go
@@ -3,8 +3,6 @@ package erb
 // FixType represents a type of navigation fix.
 type FixType uint8
 
-//go:generate gobin -m -run golang.org/x/tools/cmd/stringer -type=FixType -trimprefix=FixType
-
 const (
 	FixTypeNoFix  FixType = 0x00
 	FixTypeSingle FixType = 0x01
--- pkg/erb/svtype.go
+++ pkg/erb/svtype.go
@@ -3,8 +3,6 @@ package erb
 // SVType represents the type of an SV (space vehicle).
 type SVType uint8
 
-//go:generate gobin -m -run golang.org/x/tools/cmd/stringer -type=SVType -trimprefix=SVType
-
 const (
 	SVTypeGPS     SVType = 0
 	SVTypeGLONASS SVType = 1
